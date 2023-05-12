65900dea8d5733557635e2bc1d3aca5af39940b8
reciate <49763556+reciate@users.noreply.github.com>
2023-05-12 17:11:58 +0100

    Flying and scoreboard variables

--- Source/TBL/TBLPlayerState.h
+++ Source/TBL/TBLPlayerState.h
@@ -15,5 +15,20 @@ class TBL_API ATBLPlayerState : public APlayerState
 	GENERATED_BODY()
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TSubclassOf<APawn> NextSpawnPawnSubclass;
+	TSubclassOf<ACharacter> NextSpawnPawnSubclass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Kills;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Deaths;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Assists;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Takedown;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int GlobalRank;
 };
