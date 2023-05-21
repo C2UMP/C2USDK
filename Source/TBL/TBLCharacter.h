650b0f40a619fe02ad0e9f8db12e956f08d230d7
reciate <49763556+reciate@users.noreply.github.com>
2023-05-21 23:55:52 +0100

    Reparent BaseCharacter

---
+++ Source/TBL/TBLCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TBLCharacterBase.h"
+#include "TBLCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TBL_API ATBLCharacter : public ATBLCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TBL/TBLCharacterBase.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TBLCharacterBase.generated.h"
+
+UCLASS()
+class TBL_API ATBLCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+};
