778ccf1042468efe3cf11fe15cb1f0a7f036a6e5
reciate <49763556+reciate@users.noreply.github.com>
2023-05-22 00:10:27 +0100

    AmmoBox spawning

---
+++ Source/TBL/TBLActor.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TBLActor.generated.h"
+
+UCLASS()
+class TBL_API ATBLActor : public AActor
+{
+	GENERATED_BODY()
+};
