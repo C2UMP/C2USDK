0438f08e87cb051dcdde8bc93b75d0b523eef6b4
reciate <49763556+reciate@users.noreply.github.com>
2023-05-25 00:38:18 +0100

    Add base weapon classes and Katana

---
+++ Source/TBL/InventoryItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TBLActor.h"
+#include "InventoryItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TBL_API AInventoryItem : public ATBLActor
+{
+	GENERATED_BODY()
+	
+};
