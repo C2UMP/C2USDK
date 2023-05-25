949e6b26c5a300e599f2adf8af5f66e0d06b1168
reciate <49763556+reciate@users.noreply.github.com>
2023-05-25 21:09:39 +0100

    Rework weapon classes

--- Source/TBL/InventoryItem.cpp
+++ Source/TBL/InventoryItem.cpp
@@ -3,3 +3,18 @@
 
 #include "InventoryItem.h"
 
+AInventoryItem::AInventoryItem() {
+	this->ProjectileRoot = this->CreateDefaultSubobject<USceneComponent>(TEXT("ProjectileRoot"));
+
+	this->FirstPersonTransformRoot = this->CreateDefaultSubobject<USceneComponent>(TEXT("FirstPersonRoot"));
+	this->FirstPersonTransformRoot->AttachTo(this->ProjectileRoot);
+
+	this->ThirdPersonTransformRoot = this->CreateDefaultSubobject<USceneComponent>(TEXT("ThirdPersonRoot"));
+	this->ThirdPersonTransformRoot->AttachTo(this->ProjectileRoot);
+
+	this->ProjectileSmoothing = this->CreateDefaultSubobject<USceneComponent>(TEXT("ProjectileSmoothing"));
+	this->ProjectileSmoothing->AttachTo(this->ProjectileRoot);
+
+	this->StaticMesh = this->CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh0"));
+	this->StaticMesh->AttachTo(this->ProjectileSmoothing);
+}
--- Source/TBL/InventoryItem.h
+++ Source/TBL/InventoryItem.h
@@ -24,8 +24,17 @@ class TBL_API AInventoryItem : public ATBLActor
 {
 	GENERATED_BODY()
 public:
+	AInventoryItem();
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FInventoryItemDisplayInfo DisplayInfo;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Instanced)
+	USceneComponent* ProjectileRoot;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Instanced)
+	USceneComponent* ProjectileSmoothing;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Instanced)
+	USceneComponent* FirstPersonTransformRoot;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Instanced)
+	USceneComponent* ThirdPersonTransformRoot;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Instanced)
 	UStaticMeshComponent* StaticMesh;
 };
